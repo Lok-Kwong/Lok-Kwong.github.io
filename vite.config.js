9eb5b2a4dfb81e5336656613f08d34ca89cc9840
Lok <hk65962@uga.edu>
2025-04-01 20:34:26 -0400

    please work

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: 'https://lok-kwong.github.io/'
+  build: "lok-portfolio"
 })
